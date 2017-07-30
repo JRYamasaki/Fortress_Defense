c98b46dbc52282e3e0a1a677d603617d3b122762
JRYamasaki <yamasakijeffrey@gmail.com>
2017-07-30 16:48:13 -0700

    Add script that adds material to objects

---
+++ Assets/Scripts/AddTextures.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddTextures : MonoBehaviour 
+{
+    private GameObject[] objectsToApplyMaterialTo;
+
+    void Start()
+    {
+        Material newMat = Resources.Load("Concrete pattern 06", typeof(Material)) as Material;
+        objectsToApplyMaterialTo = GameObject.FindGameObjectsWithTag("BuildingMaterial");
+
+        foreach (GameObject obj in objectsToApplyMaterialTo)
+        {
+            obj.GetComponent<Renderer>().material = newMat;
+        }
+
+        //GameObject structure = GameObject.FindWithTag("Struct1");
+        //structure.GetComponent<Renderer>().material = newMat;
+    }
+}
