e205972d614ca409ae18567fb74b44561ca91d60
JRYamasaki <yamasakijeffrey@gmail.com>
2017-08-01 18:12:09 -0700

    Add function to AddTextures script that sets the materials for a specific tag

--- Assets/Scripts/AddTextures.cs
+++ Assets/Scripts/AddTextures.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class AddTextures : MonoBehaviour 
 {
-    private GameObject[] objectsToApplyMaterialTo;
-
     void Start()
     {
-        objectsToApplyMaterialTo = GameObject.FindGameObjectsWithTag("BuildingMaterial");
-        foreach(GameObject obj in objectsToApplyMaterialTo)
-        {
-            SetMaterialsOfChildren(obj.transform, "Concrete pattern 06");
-        }
+        SetMaterial("Concrete6", "Concrete textures pack/pattern 06/Concrete pattern 06");
+        SetMaterial("Concrete7", "Concrete textures pack/pattern 07/Concrete pattern 07");
     }
 
     void SetMaterialsOfChildren(Transform obj, string materialName)
@@ -23,12 +18,19 @@ public class AddTextures : MonoBehaviour
         }
         else //Keep looking for the children recursively
         {
-            int numChildren = obj.childCount;
-            print("Still children");
             foreach(Transform child in obj)
             {
                 SetMaterialsOfChildren(child, materialName);
             }
+        }
+    }
+
+    void SetMaterial(string tagName, string materialPath)
+    {
+        GameObject[] taggedObjects = GameObject.FindGameObjectsWithTag(tagName);
+        foreach (GameObject obj in taggedObjects)
+        {
+            SetMaterialsOfChildren(obj.transform, materialPath);
         }
     }
 }
