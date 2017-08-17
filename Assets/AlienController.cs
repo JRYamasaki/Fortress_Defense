ab1b28bcb61e720d64e0134fa0c3e610ebe7c81a
Ruben G <gonsalespalazio@gmail.com>
2017-08-17 00:44:51 -0700

    ADD enemy model (a new one); early state: i.e. the object is still controlled through player input. ADD a singleton class that abstracts the state of the scene by holding references to relevant objects (like the player) in the scene.

---
+++ Assets/AlienController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AlienController : MonoBehaviour 
+{
+	//Animator in charge of this game object's animations:
+	private Animator animator;
+	//CharacterController used to move this game object around based on user input and environment:
+	private CharacterController charController;
+
+
+
+	//Multiplier to find the speed at which rotation must occur by mulplying translation speed by it:
+	private const float TIMES_ROTATION_SPEED_IS_OF_TRANSLATION_SPEED = 16; //"A const object is always static"
+
+
+
+
+	/**The fields below are temporary and used only for the initial stages of this script or for debugging purposes.*/
+	//Used for determining what constant value to used to the speed of this game object.
+	public float speed;
+	//Used to display messages on the screen for debugging purposes:
+	public Text label;
+
+	// Use this for initialization
+	void Start () 
+	{
+		this.animator = GetComponent<Animator> ();
+		this.charController = GetComponent<CharacterController> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		//Control the game object based on user input:
+		//If user wants to displace vertically (forward or backward), do so:
+		if (Input.GetKey ("up") || Input.GetKey ("w") || Input.GetKey ("down") || Input.GetKey ("s")) {
+			verticalDisplace ();
+		} else 
+		{
+			this.animator.SetBool("isFlying", false);
+		}
+
+		//If user wants to rotate, do so:
+		if(Input.GetKey ("right") || Input.GetKey ("d") || Input.GetKey("left") || Input.GetKey("a"))
+		{
+			rotate();
+		}
+
+		//Used for debugging purposes. Testing of SceneState class. GET_RID_OF_THIS_GET_RID_OF_THIS_GET_RID_OF_THIS_GET_RID_OF_THIS_GET_RID_OF_THIS_GET_RID_OF_THIS_GET_RID_OF_THIS_GET_RID_OF_THIS_GET_RID_OF_THIS_
+		if(Input.GetKey("g"))
+		{
+			Vector3 playersPosition = SceneState.getInstance ().getPositionOfPlayer ();
+			float x = playersPosition.x, y = playersPosition.y, z = playersPosition.z;
+			string playersCoordinates = "[" + x + ", " + y + ", " + z + "]";
+			this.label.text = "The player's current coordinates are: " + playersCoordinates;
+		}
+
+		
+	}
+
+	/**Called when user input indicates this game object should be displaced forward or backward and, using such input, 
+	 * moves the game object.*/
+	private void verticalDisplace()
+	{
+		//Read user input:
+		float displacement = Input.GetAxis("Vertical");
+		//Turn on the flying animation that represents movement:
+		this.animator.SetBool("isFlying", true);
+		//Create a vector that represents the displacement to be made (note that if displacement < 0 - because the user wants to move backwards -
+		//the direction of this.transform.forward is reversed, and so indeed a backward movement is achieved):
+		Vector3 displacementVector = this.transform.forward * displacement * this.speed;
+		//Apply the movement to the game object's character controller:
+		this.charController.Move(displacementVector);
+	}
+
+	/**Called when user input indicates this game object should be rotated and, using such input, rotates the game object.*/
+	private void rotate()
+	{
+		this.label.text = "rotate method called.";
+		//Read user input:
+		float turn = Input.GetAxis("Horizontal");
+		transform.Rotate (0, turn * this.speed * AlienController.TIMES_ROTATION_SPEED_IS_OF_TRANSLATION_SPEED, 0);
+	}
+
+
+}
---
+++ Assets/SceneState.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/**This class is a representation of the game's scene, holding references to relevant game objects in it and providing accesor
+methods so game objects can interact with their scene and the objects in it. Since there is only one scene and its state is always the same at
+a given point in time, this class follows the singleton design pattern.
+@author Ruben Gonzalez.
+@lastmodified August 17th, 2017.
+*/
+public class SceneState : MonoBehaviour {
+
+	//One and only instance of the class:
+	private static SceneState instance;
+
+	/**Game objects that are relevant to the clients of this class.*/
+	//
+	public GameObject player;
+
+
+	/**Initializes this class' instance field to itself.*/
+	private SceneState()
+	{
+		SceneState.instance = this;
+	}
+
+	/**Returns the one and only instance that exists of this class.*/
+	public static SceneState getInstance()
+	{
+		if(SceneState.instance == null)
+		{
+			SceneState.instance = new SceneState ();
+		}
+		return SceneState.instance;
+	}
+
+
+	/**Returns the transform of the player game object.*/
+	public Vector3 getPositionOfPlayer()
+	{
+		return this.player.transform.position;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
